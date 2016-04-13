7d3a24b9c4b0f9a4f02e186e40215200d66c3604
MonicaCueva <mcglezoviedo@outlook.es>
2016-04-13 09:59:26 +0200

    Limpiando ficheros del modelo

--- src/test/java/es/uniovi/asw/CucumberTest.java
+++ src/test/java/es/uniovi/asw/CucumberTest.java
@@ -6,11 +6,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-// @RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(classes = Application.class)
-// @ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
-//@WebAppConfiguration
-//@IntegrationTest({ "server.port=0" })
 @CucumberOptions(features = "src/test/resources/features")
 public class CucumberTest{
 }
