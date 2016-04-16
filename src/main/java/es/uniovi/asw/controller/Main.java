660b466afdd78a76ad70f915c8b325bfe09aad33
Mónica Cueva González <mcglezoviedo@outlook.es>
2016-04-16 17:32:09 +0200

    main controller

--- src/main/java/es/uniovi/asw/controller/Main.java
+++ src/main/java/es/uniovi/asw/controller/Main.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -20,9 +21,11 @@ public class Main {
   }
   
   
-  @RequestMapping("/votar")
+  @RequestMapping(value="/votar", method=RequestMethod.POST)
   public ModelAndView votar(Model model) {
     LOG.info("Votar page access");
+    model.addAttribute("partido", new String());
+    
     return new ModelAndView("votar");
   }
   
