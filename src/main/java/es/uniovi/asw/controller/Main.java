7a2a1be9b37ab398ce425629d03d2a18c773fcfa
Mónica Cueva González <mcglezoviedo@outlook.es>
2016-04-18 13:27:10 +0200

    mostrar errores si no se rellenan campos

--- src/main/java/es/uniovi/asw/controller/Main.java
+++ src/main/java/es/uniovi/asw/controller/Main.java
@@ -16,6 +16,7 @@ import es.uniovi.asw.dbupdate.ColegioRepository;
 import es.uniovi.asw.dbupdate.VoterRepository;
 import es.uniovi.asw.dbupdate.VotoRepository;
 import es.uniovi.asw.modelo.ColegioElectoral;
+import es.uniovi.asw.modelo.Elecciones;
 import es.uniovi.asw.modelo.PartidoPolitico;
 import es.uniovi.asw.modelo.Voto;
 
@@ -28,9 +29,9 @@ public class Main {
 	VotoRepository votoRepository = null;
 	@Autowired
 	VoterRepository voterRepository = null;
-	
+
 	@Autowired
-	ColegioRepository colegioRepository=null;
+	ColegioRepository colegioRepository = null;
 
 	@RequestMapping("/")
 	public ModelAndView landing(Model model) {
@@ -53,55 +54,124 @@ public class Main {
 	@RequestMapping(value = "/votar", method = RequestMethod.POST)
 	public String saveVote(Voto voto, Model model) {
 
+		List<PartidoPolitico> partidos = new ArrayList<PartidoPolitico>();
 		for (PartidoPolitico p : PartidoPolitico.values()) {
-			if (p.equals(voto.getPartidoPolitico())) {
+			partidos.add(p);
+		}
+		model.addAttribute("partidos", partidos);
+
+		if (voto.getPartidoPolitico() == null) {
+			Voto v = new Voto(null, null, false, false, true);
+			votoRepository.save(v);
+			model.addAttribute("mensaje",
+					"Ha votado correctamente: voto en blanco");
+			LOG.info("Se ha añadido un nuevo voto en blanco");
+			// voterRepository.setEjercioDerechoAlVotoFor(true,
+			// voter.getNif());
+			return "/votar";
+		}
+
+		for (PartidoPolitico p : PartidoPolitico.values()) {
+			if (p.toString().equals(voto.getPartidoPolitico().toString())) {
 				Voto v = new Voto(null, p, false, false, false);
 				votoRepository.save(v);
+				model.addAttribute("mensaje", "Ha votado correctamente");
+				LOG.info("Se ha añadido un nuevo voto");
 				// voterRepository.setEjercioDerechoAlVotoFor(true,
 				// voter.getNif());
-
-			} else if (voto.getPartidoPolitico().equals("")) {
-				Voto v = new Voto(null, null, false, false, true);
-				votoRepository.save(v);
-				// voterRepository.setEjercioDerechoAlVotoFor(true,
-				// voter.getNif());
-			} else {
-				Voto v = new Voto(null, null, false, true, false);
-				votoRepository.save(v);
-				// voterRepository.setEjercioDerechoAlVotoFor(true,
-				// voter.getNif());
+				return "/votar";
 			}
 		}
-		return "index";
+		if (voto.getPartidoPolitico().equals("")) {
+			Voto v = new Voto(null, null, false, false, true);
+			votoRepository.save(v);
+			model.addAttribute("mensaje",
+					"Ha votado correctamente: voto en blanco");
+			LOG.info("Se ha añadido un nuevo voto en blanco");
+			// voterRepository.setEjercioDerechoAlVotoFor(true,
+			// voter.getNif());
+			return "/votar";
+
+		} else {
+			Voto v = new Voto(null, null, false, true, false);
+			votoRepository.save(v);
+			model.addAttribute("mensaje", "Ha votado correctamente: voto nulo");
+			LOG.info("Se ha añadido un nuevo voto nulo");
+			// voterRepository.setEjercioDerechoAlVotoFor(true,
+			// voter.getNif());
+			return "/votar";
+		}
+
 	}
 
-	@RequestMapping("/modificar_elecciones")
-	public ModelAndView modificar(Model model) {
+	@RequestMapping(value= "/modificar_elecciones")
+	public String modificar(Model model) {
 		LOG.info("Modificar elecciones page access");
-		return new ModelAndView("modificar_elecciones");
+		return "/modificar_elecciones";
 	}
+	
 
+	@RequestMapping(value = "/modificar_elecciones", method = RequestMethod.POST)
+	public String addElecciones(Elecciones eleccion, Model model) {	
+		LOG.info("Modificar elecciones page access");
+		return "/modificar_elecciones";
+	}
 	
-	
+
 	@RequestMapping(value = "/add_colegio")
 	public String partido(ColegioElectoral colegioElectoral, Model model) {
+		LOG.info("Add school page access");
 		return "/add_colegio";
 	}
-	
+
 	@RequestMapping(value = "/add_colegio", method = RequestMethod.POST)
 	public String addPartido(ColegioElectoral colegioElectoral, Model model) {
-		
-		List<ColegioElectoral> colegios = colegioRepository.findAll();
-		for (ColegioElectoral colegioElectoral2 : colegios) {
-			if(colegioElectoral.getCodigoColegio()!= colegioElectoral2.getCodigoColegio()){
-				ColegioElectoral c = new ColegioElectoral(colegioElectoral.getCodigoColegio(),
-						colegioElectoral.getCircunscripcion(), colegioElectoral.getComunidadAutonoma());
+
+		if (colegioElectoral.getCircunscripcion() == null
+				|| colegioElectoral.getComunidadAutonoma() == null) {
+			model.addAttribute("mensaje",
+					"Por favor, rellene todos los campos");
+			LOG.info("No se ha añadido ningún colegio electoral");
+			return "/add_colegio";
+		} else {
+
+			List<ColegioElectoral> colegios = colegioRepository.findAll();
+			if (colegios.isEmpty()) {
+				ColegioElectoral c = new ColegioElectoral(
+						colegioElectoral.getCodigoColegio(),
+						colegioElectoral.getCircunscripcion(),
+						colegioElectoral.getComunidadAutonoma());
 				colegioRepository.save(c);
+				model.addAttribute("mensaje",
+						"Se ha añadido el nuevo colegio electoral con código: "
+								+ c.getCodigoColegio());
+				LOG.info("Se ha añadido un nuevo colegio electoral");
+			} else {
+				for (ColegioElectoral colegioElectoral2 : colegios) {
+					if (colegioElectoral.getCodigoColegio() != colegioElectoral2
+							.getCodigoColegio()) {
+						ColegioElectoral c = new ColegioElectoral(
+								colegioElectoral.getCodigoColegio(),
+								colegioElectoral.getCircunscripcion(),
+								colegioElectoral.getComunidadAutonoma());
+						colegioRepository.save(c);
+						model.addAttribute("mensaje",
+								"Se ha añadido el nuevo colegio electoral con código: "
+										+ c.getCodigoColegio());
+						LOG.info("Se ha añadido un nuevo colegio electoral");
+						return "/add_colegio";
+
+					} else {
+						model.addAttribute("mensaje",
+								"No se ha podido añadir el colegio electoral");
+						LOG.info("Error, no se ha podido añadir el colegio electoral");
+						return "/add_colegio";
+
+					}
+				}
 			}
+			return "/add_colegio";
 		}
-
-		return "modificar_elecciones";
 	}
-
 
 }
