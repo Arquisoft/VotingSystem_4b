0bf8dd24e069cc85ccfe331e9da8f1fdd71bb947
Mónica Cueva González <mcglezoviedo@outlook.es>
2016-04-19 17:42:07 +0200

    pruebas unitarias controller

--- src/test/java/es/uniovi/asw/MainControllerTest.java
+++ src/test/java/es/uniovi/asw/MainControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
@@ -42,18 +43,29 @@ public class MainControllerTest {
   @Test
   public void testVotar() throws Exception {
     mvc.perform(get("/votar")).andExpect(status().isOk()).andExpect(content().string(containsString("Votar")));
+    mvc.perform(get("/votar")).andExpect(status().isOk()).andExpect(content().string(containsString("Partido")));
+    mvc.perform(get("/votar")).andExpect(status().isOk()).andExpect(content().string(containsString("VOTAR")));
   }
+  
   
   @Test
   public void testModificar() throws Exception {
     mvc.perform(get("/modificar_elecciones")).andExpect(status().isOk()).andExpect(content().string(containsString("Modificar elecciones")));
+    mvc.perform(get("/modificar_elecciones")).andExpect(status().isOk()).andExpect(content().string(containsString("Nombre de las elecciones")));
+    mvc.perform(get("/modificar_elecciones")).andExpect(status().isOk()).andExpect(content().string(containsString("Opciones de voto")));
+    mvc.perform(get("/modificar_elecciones")).andExpect(status().isOk()).andExpect(content().string(containsString("AÑADIR COLEGIO ELECTORAL")));
+    mvc.perform(get("/modificar_elecciones")).andExpect(status().isOk()).andExpect(content().string(containsString("MODIFICAR")));
   }
   
   @Test
   public void testColegio() throws Exception {
     mvc.perform(get("/add_colegio")).andExpect(status().isOk()).andExpect(content().string(containsString("Añadir colegio electoral")));
+    mvc.perform(get("/add_colegio")).andExpect(status().isOk()).andExpect(content().string(containsString("Circunscripción")));
+    mvc.perform(get("/add_colegio")).andExpect(status().isOk()).andExpect(content().string(containsString("Comunidad Autónoma")));
+    mvc.perform(get("/add_colegio")).andExpect(status().isOk()).andExpect(content().string(containsString("AÑADIR")));
   }
   
+
   
   
   
